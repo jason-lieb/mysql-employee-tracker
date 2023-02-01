8b3c918566166571c1aec5891102dbab4a349e57
Jason Lieb <Jason.lieb@outlook.com>
2023-02-01 13:44:50 -0500

    fix syntax of schema and seed sql files

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,21 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-  id PRIMARY KEY INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-  id PRIMARY KEY INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  department_id INT NOT NULL,
-  salary DECIMAL NOT NULL,
-  FOREIGN KEY(department_id),
-  REFERENCES department(id)
+  department_id INT NOT NULL REFERENCES department(id),
+  salary DECIMAL NOT NULL
 );
 
 CREATE TABLE employee(
-  id PRIMARY KEY INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  FOREIGN KEY(role_id),
-  REFERENCES role(id),
-  FOREIGN KEY(manager_id),
-  REFERENCES employee(id)
+  role_id INT NOT NULL REFERENCES role(id),
+  manager_id INT REFERENCES employee(id)
 );
